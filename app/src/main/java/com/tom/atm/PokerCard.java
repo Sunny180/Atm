6e051a2316ce06ca468782dddf09998970f410bf
miia <miia@com.tw>
2019-12-10 16:27:51 +0800

    1 commit

---
+++ app/src/main/java/com/tom/atm/PokerCard.java
@@ -0,0 +1,11 @@
+package com.tom.atm;
+
+import android.content.Context;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class PokerCard extends ConstraintLayout {
+    public PokerCard(Context context) {
+        super(context);
+    }
+}
